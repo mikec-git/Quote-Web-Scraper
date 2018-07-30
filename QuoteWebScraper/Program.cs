3f3a91225f6778ef08eba6125bb0a13154b6eb35
Mike Choi <mike.choi@live.ca>
2018-07-29 23:09:39 -0700

    Created solution in VS

---
+++ QuoteWebScraper/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AngleSharp;
+using AngleSharp.Parser.Html;
+
+namespace QuoteWebScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var parser = new HtmlParser();
+
+            var webSource = "https://www.goodreads.com/quotes/tag?utf8=%E2%9C%93&id=motivational";
+
+            var document = parser.Parse(webSource);
+
+
+        }
+    }
+}
