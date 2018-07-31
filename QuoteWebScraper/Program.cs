471c0aebbc7582185633c4510efeeab4ff8eb412
Mike Choi <mike.choi@live.ca>
2018-07-30 17:48:21 -0700

    User can search for quotes based on keyword and the number of result pages to display

--- QuoteWebScraper/Program.cs
+++ QuoteWebScraper/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Net.Http;
+//using System.Net.Http;
 using HtmlAgilityPack;
 
 namespace QuoteWebScraper
@@ -12,55 +12,87 @@ namespace QuoteWebScraper
     {
         static void Main(string[] args)
         {
-            //string url = "https://www.brainyquote.com/topics/motivational?lgm=l";
-            string url = "https://www.goodreads.com/quotes/";
+            // Scraping from https://goodreads.com for desired quotes
+            string url = "https://www.goodreads.com/quotes";
 
-            Console.WriteLine("Generate by tag (separate by spaces): ");
+            Console.WriteLine("Generate by keyword (separate with spaces or commas): ");
             string keywords = Console.ReadLine();
 
             Console.WriteLine("Enter the number of pages to query: ");
-            string numberOfPagesString = Console.ReadLine();
+            string numberOfPagesString = null; ;
+            do
+            {
+                numberOfPagesString = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(numberOfPagesString))
+                    numberOfPagesString = "0";
+
+            } while (string.IsNullOrEmpty(numberOfPagesString));
+
             int numOfPages = Convert.ToInt32(numberOfPagesString);
+            if (numOfPages <= 0) numOfPages = 1;
 
+            if (!string.IsNullOrEmpty(keywords))
+            {
+                char[] delim = new char[] { ' ', ',' };
+                string[] keywordList = keywords.Split(delim, StringSplitOptions.RemoveEmptyEntries);
+                int numOfKeywords = keywordList.Length;
+
+                url = url + $"/tag?id={keywordList[0]}";
+
+                if (numOfKeywords > 1)
+                {
+                    for (int i = 1; i < numOfKeywords; i++)
+                        url = url + $"+{keywordList[i]}";
+                }
+
+                url = url + "&page=";
+            }
+            else { url = url + "?page="; }
+
+            GetHtmlAsync(url, numOfPages);
             
-
-            GetHtmlAsync(url);
-            
-
             Console.ReadKey();
         }
 
-        private static async Task GetHtmlAsync(string url)
+        private static async Task GetHtmlAsync(string url, int pages)
         {
             QuoteData quoteData = new QuoteData();
             HtmlWeb web = new HtmlWeb();
 
-            var htmlDocument = await web.LoadFromWebAsync(url);
+            string urlPage;
 
-            // Stores the specified descendants for the quotes and authors
-            var quoteHtml = htmlDocument.DocumentNode.Descendants("div")
-                .Where(node => node.GetAttributeValue("class", "").Equals("quoteText")).ToList();
-
-            var authorHtml = htmlDocument.DocumentNode.Descendants("a")
-                .Where(node => node.GetAttributeValue("href", "").Contains("/author/") 
-                && node.GetAttributeValue("class","").Contains("authorOrTitle")).ToList();
-
-            int listLength = quoteHtml.Count;
-
-            // The quotes on this URI get converted into the following strings
-            string[] quoteDelim = new string[] { "&ldquo;", "&rdquo;" };
-            string extractedQuote;
-
-            for (int i = 0; i < listLength; i++)
+            for (int i = 1; i <= pages; i++)
             {
-                extractedQuote = quoteHtml[i].InnerHtml
-                    .Split(quoteDelim, StringSplitOptions.None)[1]
-                    .Replace("<br>", "\n");
+                urlPage = url + $"{i}&utf8=✓";
 
-                quoteData.quotes.Add("\"" + extractedQuote + "\"");
-                quoteData.authors.Add(authorHtml[i].InnerText);
+                var htmlDocument = await web.LoadFromWebAsync(urlPage);
+
+                // Stores the specified descendants for the quotes and authors
+                var quoteHtml = htmlDocument.DocumentNode.Descendants("div")
+                    .Where(node => node.GetAttributeValue("class", "").Equals("quoteText")).ToList();
+
+                var authorHtml = htmlDocument.DocumentNode.Descendants("a")
+                    .Where(node => node.GetAttributeValue("href", "").Contains("/author/")
+                    && node.GetAttributeValue("class", "").Contains("authorOrTitle")).ToList();
+
+                int listLength = quoteHtml.Count;
+
+                // Double quotes for this URL show up as the following deliminators
+                string[] quoteDelim = new string[] { "&ldquo;", "&rdquo;" };
+                string extractedQuote;
+
+                for (int j = 0; j < listLength; j++)
+                {
+                    extractedQuote = quoteHtml[j].InnerHtml
+                        .Split(quoteDelim, StringSplitOptions.None)[1]
+                        .Replace("<br>", "\n");
+
+                    quoteData.quotes.Add("\"" + extractedQuote + "\"");
+                    quoteData.authors.Add(authorHtml[j].InnerText);
+                }
             }
-            
+
             Console.WriteLine();
         }
     }
