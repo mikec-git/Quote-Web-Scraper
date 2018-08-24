2f35ae5c5c3dddc1e6efb1511176b95c4d682929
Mike Choi <mike.choi@live.ca>
2018-08-23 20:48:52 -0700

    Refactored UserInput class to make it more concise

--- QuoteWebScraper/UserInput.cs
+++ QuoteWebScraper/UserInput.cs
@@ -13,81 +13,62 @@ namespace QuoteWebScraper
 
         public static void GetPages(PageAndUrl pageAndUrl, string numOfPagesString)
         {
-            numOfPagesString = PageEmptyCheck(numOfPagesString);
+            numOfPagesString = NoPageInputCheck(numOfPagesString);
+
             string[] pageRangeString = numOfPagesString.Split(stringSplit, StringSplitOptions.RemoveEmptyEntries);
+            List<string> pageRange = new List<string>() { pageRangeString[0] };
 
-            // Getting Page Numbers to Query
-            if (pageRangeString.Length > 1)
+            int pageRangeStrlength = pageRangeString.Length;
+
+            if (pageRangeStrlength > 1)
             {
-                pageAndUrl.pages = NumOfPagesArray(pageRangeString);
-
-                if (pageAndUrl.pages.Distinct().Count() == 1)
+                for (int i = 0; i < pageRangeStrlength; i++)
                 {
-                    pageAndUrl.pages = new List<int> { pageAndUrl.pages.Distinct().First() };
-                }
-            }
-            else
-            {
-                int page = Convert.ToInt32(pageRangeString.First());
-
-                if (page <= 0)
-                {
-                    pageRangeString[0] = "1";
-                }
-
-                pageAndUrl.pages = new List<int> { Convert.ToInt32(pageRangeString[0]) };
-            }
-        }
-
-        public static void GetUrl(PageAndUrl pageAndUrl, string keywords)
-        { 
-            // Getting URL Stem
-            if (!string.IsNullOrEmpty(keywords))
-            {
-                string[] keywordList = keywords.Split(delim, StringSplitOptions.RemoveEmptyEntries);
-                int numOfKeywords = keywordList.Length;
-
-                pageAndUrl.url = pageAndUrl.url + $"/tag?id={keywordList[0]}";
-
-                if (numOfKeywords > 1)
-                {
-                    for (int i = 1; i < numOfKeywords; i++)
+                    if (!pageRange[0].Equals(pageRangeString[i]))
                     {
-                        pageAndUrl.url = pageAndUrl.url + $"+{keywordList[i]}";
+                        pageRange.Add(pageRangeString[i]);
+                        break;
                     }
                 }
-
-                pageAndUrl.url = pageAndUrl.url + "&page=";
             }
-            else
-            {
-                pageAndUrl.url = pageAndUrl.url + "?page=";
-            }
-        }
 
-        private static string PageEmptyCheck(string numOfPagesString)
-        {
-            if (string.IsNullOrEmpty(numOfPagesString))
-            {
-                return "1";
-            }
-            else
-            {
-                return numOfPagesString;
-            }
-        }
+            List<int> pages = pageRange.ConvertAll(x => Convert.ToInt32(x));
 
-        private static List<int> NumOfPagesArray(string[] pageRangeString)
-        {
-            List<string> pageRangeTwo = pageRangeString.Take(2).ToList();
-            List<int> pages = pageRangeTwo.ConvertAll(x => Convert.ToInt32(x));
-
-            for (int i = 0; i < pages.Count; i++)
+            for (int i = 0; i < pageRange.Count; i++)
             {
                 if (pages[i] <= 0) pages[i] = 1;
             }
 
-            return pages;
+            pageAndUrl.pages = pages.Distinct().ToList();
         }
+
+        public static void GetUrl(PageAndUrl pageAndUrl, string keywords)
+        {
+            StringBuilder newUrl = new StringBuilder();
+            newUrl.Append(pageAndUrl.url);
+
+            // Getting URL Stem
+            if (!string.IsNullOrEmpty(keywords))
+            {
+                string[] keywordList = keywords.Split(delim, StringSplitOptions.RemoveEmptyEntries);
+                int numOfKeywords    = keywordList.Length;
+
+                newUrl.Append($"/tag?id={keywordList[0]}");
+
+                if (numOfKeywords > 1)
+                {
+                    for (int i = 1; i < numOfKeywords; i++)
+                        newUrl.Append($"+{keywordList[i]}");
+                }
+            }
+            newUrl.Append("&page=");
+            pageAndUrl.url = newUrl.ToString();
+        }
+
+        private static string NoPageInputCheck(string numOfPagesString)
+        {
+            if (string.IsNullOrEmpty(numOfPagesString)) return "1";
+            else                                        return numOfPagesString;
+        }        
     }
 }
